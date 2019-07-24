d3153d6ec89a0c2409479816cb9e6f945b34fc10
Gxbx <gado.dev@outlook.com>
2019-07-24 17:10:35 -0500

    add pull up down function

--- experiments/17_hello_raspi/17_hello_raspi.py
+++ experiments/17_hello_raspi/17_hello_raspi.py
@@ -1,16 +1,16 @@
 import RPi.GPIO as gpio 
 import time
 #Esto es para iniciar la board
-gpio.setmode(gpio.BOARD)
+gpio.setmode(gpio.BCM)
+gpio.setup(23, gpio.IN, pull_up_down = gpio.PUD_UP)
 gpio.setup(24, gpio.OUT)
-gpio.setup(23, gpio.IN)
-# gpio.output(24, True)
-# time.sleep(20)
-# gpio.output(24, False)
-# gpio.cleanup(24)
+
 while True:
     input_state = gpio.input(23)
     if input_state == False:
         gpio.output(24, True)
     else:
         gpio.output(24, False)
+time.sleep(0.3)
+gpio.cleanup(23)
+gpio.cleanup(24)
